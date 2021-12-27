4b17e6b5c1fe26d308b75b7b459d1de2abb735ec
chentk <chentk@ligeit.com>
2021-12-27 15:44:44 +0800

    test no

--- test/src/test/java/MainTest.java
+++ test/src/test/java/MainTest.java
@@ -1,6 +1,8 @@
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.UUID;
 
 @Slf4j
@@ -8,6 +10,27 @@ public class MainTest {
     public static void main(String[] args) {
 //        uuid();
 
+//        parseLong();
+
+        appendNo();
+    }
+
+    private static void appendNo() {
+        while (new Random().nextInt(20) > 5) {
+            StringBuilder orderNo = new StringBuilder();
+            orderNo.append("PO");
+            long curTimeMillis = System.currentTimeMillis();
+            System.out.println("curTimeMillis -> " + curTimeMillis);
+            orderNo.append(curTimeMillis);
+            String userIdString = "00000" + RandomStringUtils.randomNumeric(new Random().nextInt(20));
+            orderNo.append(userIdString.substring(userIdString.length() - 4));
+            String randomNumber = "00" + new Random().nextInt(100);
+            orderNo.append(randomNumber.substring(randomNumber.length() - 2));
+            System.out.println("No -> " + orderNo.toString() + "\n");
+        }
+    }
+
+    private static void parseLong() {
         int nm = 0;
         while (nm++ <= 10) {
             System.out.println(nm + "|" + Long.parseLong(nm + ""));
